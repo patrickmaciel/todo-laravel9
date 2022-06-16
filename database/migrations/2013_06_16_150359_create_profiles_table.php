887de3331cda15c768a6b163d076d7c6c656ef59
Patrick Maciel Campos <patrickmaciel.info@gmail.com>
2022-06-16 12:14:59 -0300

    added profiles migration and fix user migration

--- database/migrations/2013_06_16_150359_create_profiles_table.php
+++ database/migrations/2013_06_16_150359_create_profiles_table.php
@@ -13,15 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            // $table->foreign('profile_id')->references('id')->on('profiles');
-            $table->foreignId('profile_id')->constrained('profiles');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 80)->index();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -34,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('profiles');
     }
 };
